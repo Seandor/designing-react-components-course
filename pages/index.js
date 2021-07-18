827df43dab8c95978de1bef7d5f26d1b18874ec4
Seandor <seandor123@gmail.com>
2021-07-18 10:29:33 +0800

    finish module1

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
+import { data } from "../SpeakerData";
+import Speaker from "../src/components/Speaker"
+
 const IndexPage = () => {
   return (
-    <div>
-      Hello From Microsoft!
+    <div className="container speakers-list">
+      <div className="row">
+        {data.map(function (speaker) {
+          return (
+            <Speaker key={speaker.id} speaker={speaker} />
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
